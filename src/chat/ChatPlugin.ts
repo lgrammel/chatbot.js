3e3a94f562695138bd0e464f0ebb9ac88311bf22
Lars Grammel <lars.grammel@gmail.com>
2023-02-06 21:10:28 +0100

    Add GET /chat/:id route. Save chats.

--- src/chat/ChatPlugin.ts
+++ src/chat/ChatPlugin.ts
@@ -1,10 +1,32 @@
 import { FastifyInstance } from "fastify";
+import zod from "zod";
+import { createNextId } from "../util/createNextId";
 
 export const ChatPlugin = () =>
   async function plugin(server: FastifyInstance) {
     const logger = server.log;
 
+    const nextId = createNextId();
+    const chats = new Map<string, true>();
+
     server.post("/chat", async function (request, reply) {
-      logger.info("CREATE CHAT");
+      const chatId = nextId();
+      chats.set(chatId, true);
+      return { id: chatId };
+    });
+
+    server.get("/chat/:id", async function (request, reply) {
+      const parameterSchema = zod.object({
+        id: zod.string(),
+      });
+
+      const { id } = parameterSchema.parse(request.params);
+
+      if (!chats.has(id)) {
+        reply.code(404);
+        return;
+      }
+
+      return { id };
     });
   };
