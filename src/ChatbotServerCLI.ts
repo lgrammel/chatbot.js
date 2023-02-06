995b857a7ce64507def0f82f585a3a17fc353062
Lars Grammel <lars.grammel@gmail.com>
2023-02-06 20:49:32 +0100

    Start Fastify server.

--- src/ChatbotServerCLI.ts
+++ src/ChatbotServerCLI.ts
@@ -1,15 +1,39 @@
+import { Option } from "commander";
+import Fastify from "fastify";
+import hyperid from "hyperid";
 import zod from "zod";
 import { startService } from "./program/startService";
 
 startService({
   name: "Chatbot server",
-  configurationOptions: [],
-  configurationSchema: zod.object({}),
+  configurationOptions: [
+    new Option("--port <number>", "port number")
+      .env("PORT")
+      .argParser((value) => +value)
+      .makeOptionMandatory(),
+    new Option("--host <string>", "host name")
+      .env("HOST")
+      .makeOptionMandatory(),
+  ],
+  configurationSchema: zod.object({ host: zod.string(), port: zod.number() }),
   async initialize(configuration, logger) {
-    logger.info("Hello world!");
+    const server = Fastify({
+      logger,
+
+      // Generate custom request IDs. This is required to distinguish between
+      // requests made on different pods in a Kubernetes cluster.
+      genReqId: hyperid(),
+
+      // Unified key for the request ID in the logs:
+      requestIdLogLabel: "requestId",
+    });
+
+    await server.listen({ host: configuration.host, port: configuration.port });
 
     return {
-      async shutdown() {},
+      async shutdown() {
+        await server.close(); // wait for requests to be finished
+      },
     };
   },
 });
